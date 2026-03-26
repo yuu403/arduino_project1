fd9f0505c0592c1d452d5bbdf54169d66f97ff4d
yuu403 <yyyusuke20@gmail.com>
2026-03-26 16:56:39 +0900

    Add files via upload

---
+++ Ltika_copy_20260326165354.ino
@@ -0,0 +1,39 @@
+unsigned long startTime = 0;
+bool ledState = false;
+bool switchState = false;
+bool lastswitchState = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, INPUT_PULLUP);
+  pinMode(7, OUTPUT);
+  pinMode(11, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A0);
+  Serial.println(val);
+
+  if (digitalRead(3) == LOW) {  // 押すとLOW
+    if (!lastswitchState) switchState = !switchState;
+    lastswitchState = true;
+  } else {
+    lastswitchState = false;
+  }
+
+  if (switchState){
+    unsigned long now = millis();
+
+    if (now - startTime >= 1000) {  // 1秒経過
+      ledState = !ledState;         //
+      digitalWrite(7, ledState);
+      startTime = now;  // 更新
+    }
+  } else {
+    digitalWrite(7, LOW);
+    ledState = false;
+    startTime = millis();
+  }
+}
